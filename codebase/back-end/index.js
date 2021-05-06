70c28dc93099c7891762bcf1dc402d5ce8dade75
Waynar Bocangel Calderon <waynarbocangel@gmail.com>
2021-05-06 19:51:36 -0400

    MongoDB setup with mongoose and MongoDB Atlas

--- codebase/back-end/index.js
+++ codebase/back-end/index.js
@@ -1,6 +1,18 @@
+require("dotenv").config();
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+mongoose.connect(process.env.DB, { useUnifiedTopology: true, useNewUrlParser: true});
+
+mongoose.set("useCreateIndex", true);
+
+let clientSchema = new mongoose.Schema({
+	email: String,
+	password: String
+});
+
+const Client = new mongoose.model("Client", clientSchema);
 
 app.get("/", (request, response) => {
 	let man = {
