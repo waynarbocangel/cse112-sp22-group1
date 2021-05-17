00c07007adcc0eb4f14be310950a08e18e123f3d
darres heyes <56111676+partimeVillain@users.noreply.github.com>
2021-05-16 18:04:28 -0700

    deleted testing

--- codebase/back-end/testing.js
+++
@@ -1,7 +0,0 @@
-const cryp = require("./cryptotest.js");
-//import { passHash } from './cryptotest.js';
-//const crtp = passHash(import.meta.url);
-
-//console.log(cryp.passHash("hello"));
-//console.log(cryp.encrypt("liu is the best"));
-console.log(cryp.decrypt(cryp.encrypt("liu is the best")));
