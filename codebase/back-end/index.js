1fe8cba1f672bb0b46eb2984ba6945ef285271e8
carlos <cguerrer@ucsd.edu>
2021-05-16 17:56:17 -0700

    index.js compile error fixed

--- codebase/back-end/index.js
+++ codebase/back-end/index.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
-const e = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
-const deleteUser = require(__dirname + "/deleteFiles/deleteUser");
+const deleteUser = require(__dirname + "/deleteFiles/deleteUser.js");
 const createUser = require(__dirname + "/createFiles/createUser.js");
 const updateUser = require(__dirname + "/updateFiles/updateUser.js");
 const readUser = require(__dirname + "/readFiles/readUser.js");
