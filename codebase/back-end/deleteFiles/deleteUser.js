b8c0fa758fce6482f24cd66d4f131a43df27e484
carlos <cguerrer@ucsd.edu>
2021-05-18 12:43:28 -0700

    delete and create user debugging

--- codebase/back-end/deleteFiles/deleteUser.js
+++ codebase/back-end/deleteFiles/deleteUser.js
@@ -5,29 +5,8 @@ const schema = require(__dirname + "/../schema.js");
 mongoose.connect(process.env.DB, {useUnifiedTopology: true, useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
-<<<<<<< HEAD
-function deleteUserPouch(db, userObject, callback) {
-	db.get("0000", (err, doc) => {
-		if (err) {
-			callback(err);
-		} else {
-			db.remove(doc, function(err, response) {
-				if (err) {
-					callback(err);
-				} else {
-					callback(response);
-				}
-			});
-		}
-	})
-}
-
-function deleteUser (db, userObject, callback) {
-	schema.User.deleteOne({email: userObject.email}, (error, user) => {
-=======
 function deleteUser (userObject, callback) {
 	schema.User.findOneAndDelete({email: userObject.email}, (error, user) => {
->>>>>>> back-end_drop
 		if (error) {
 			callback(error);
 		} else {
@@ -35,9 +14,9 @@ function deleteUser (userObject, callback) {
 		}
 	});
 	
-	deleteUserPouch(db, userObject, (res) => {
+	/*deleteUserPouch(db, userObject, (res) => {
 		console.log(res);
-	});
+	});*/
 }
 
 module.exports = {
