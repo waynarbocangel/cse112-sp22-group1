dbcf447222e30fc332e85979c869c96c26b035ea
carlos <cguerrer@ucsd.edu>
2021-05-18 16:38:06 -0700

    eslint changes to schema.js

--- codebase/back-end/schema.js
+++ codebase/back-end/schema.js
@@ -6,66 +6,84 @@ mongoose.set("useCreateIndex", true);
 const userSchema = {
 	email: String,
 	pwd: String,
-	dailyLogs: [{
-		id: String,
-		date: Date,
-		parent: String,
-		content: [String],
-		trackers: [String]
-	}],
-	monthlyLogs: [{
-		id: String,
-		parent: String,
-		date: Date,
-		content: [String],
-		days: [String],
-		trackers: [String]
-	}],
-	futureLogs:[{
-		id: String,
-		startDate: Date,
-		endDate: Date,
-		months: [String],
-		content: [String],
-		trackers: [String]
-	}],
-	trackers: [{
-		id: String,
-		content: [String],
-		parent: String
-	}],
-	collections: [{
-		id: String,
-		title: String,
-		parent: String,
-		content: [String]
-	}],
-	textBlocks: [{
-		id: String,
-		parent: String,
-		kind: String,
-		text: String,
-		signifier: String
-	}],
-	eventBlocks: [{
-		id: String,
-		parent: String,
-		text: String,
-		date: Date,
-		signifier: String
-	}],
-	taskBlocks: [{
-		id: String,
-		parent: String,
-		text: String,
-		complete: Number,
-		signifier: String
-	}],
-	signifiers: [{
-		id: String,
-		meaning: String,
-		symbol: String
-	}]
+	dailyLogs: [
+		{
+			id: String,
+			date: Date,
+			parent: String,
+			content: [String],
+			trackers: [String]
+		}
+	],
+	monthlyLogs: [
+		{
+			id: String,
+			parent: String,
+			date: Date,
+			content: [String],
+			days: [String],
+			trackers: [String]
+		}
+	],
+	futureLogs: [
+		{
+			id: String,
+			startDate: Date,
+			endDate: Date,
+			months: [String],
+			content: [String],
+			trackers: [String]
+		}
+	],
+	trackers: [
+		{
+			id: String,
+			content: [String],
+			parent: String
+		}
+	],
+	collections: [
+		{
+			id: String,
+			title: String,
+			parent: String,
+			content: [String]
+		}
+	],
+	textBlocks: [
+		{
+			id: String,
+			parent: String,
+			kind: String,
+			text: String,
+			signifier: String
+		}
+	],
+	eventBlocks: [
+		{
+			id: String,
+			parent: String,
+			text: String,
+			date: Date,
+			signifier: String
+		}
+	],
+	taskBlocks: [
+		{
+			id: String,
+			parent: String,
+			text: String,
+			complete: Number,
+			signifier: String
+		}
+	],
+	signifiers: [
+		{
+			id: String,
+			meaning: String,
+			symbol: String
+		}
+	]
 };
 
 const User = new mongoose.Schema(userSchema);
