3dc52c7bd6df2c377dd86d2dd72907006e7f25b1
carlos <cguerrer@ucsd.edu>
2021-05-20 01:55:59 -0700

    all delete files for pouch have been finished

--- codebase/front-end/localStorage/deleteFiles/deleteTracker.js
+++ codebase/front-end/localStorage/deleteFiles/deleteTracker.js
@@ -1,33 +1,16 @@
-export function deleteUserPouch(db, callback) {
+export function deleteTrackersPouch(db, id, callback) {
 	db.get("0000", (err, doc) => {
 		if (err) {
 			callback(err);
 		} else {
-			db.remove(doc, function(err, response) {
+			const newTrackers = doc.trackers.filter(tracker => tracker.id != id);
+			db.put({_id: "0000", _rev: doc._rev, trackers: newTrackers}, (err, res) => {
 				if (err) {
 					callback(err);
 				} else {
-					callback(response);
+					callback(res);
 				}
 			});
 		}
 	})
-	db.put({_id: "0000", _rev: userObject._rev,
-		email: userObject.email,
-		pwd: userObject.pwd,
-		dailyLogs: userObject.dailyLogs, 
-		monthlyLogs: userObject.monthlyLogs,
-		futureLogs: userObject.futureLogs,
-		collections: userObject.collections,
-		trackers: userObject.trackers,
-		textBlocks: userObject.textBlocks,
-		taskBlocks: userObject.taskBlocks,
-		eventBlocks: userObject.eventBlocks,
-		signifiers: userObject.signifiers}, (err, res) => {
-		if (err) {
-			callback(err);//err when i use callback
-		} else {
-			callback(res);//err when i use callback
-		}
-	});
 }
