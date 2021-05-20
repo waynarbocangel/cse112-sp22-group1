809acb894c2d8a70f862c0320d5dac18218bb19b
carlos <cguerrer@ucsd.edu>
2021-05-20 01:48:16 -0700

    finished delete collection and eventBlock files

---
+++ codebase/front-end/localStorage/deleteFiles/deleteEventBlock.js
@@ -0,0 +1,16 @@
+export function deleteEventBlockPouch(db, id, callback) {
+	db.get("0000", (err, doc) => {
+		if (err) {
+			callback(err);
+		} else {
+			const newEventBlocks = doc.eventBlocks.filter(eventBlock => eventBlock.id != id);
+			db.put({_id: "0000", _rev: doc._rev, collections: newEventBlocks}, (err, res) => {
+				if (err) {
+					callback(err);//err when i use callback
+				} else {
+					callback(res);//err when i use callback
+				}
+			});
+		}
+	})
+}
