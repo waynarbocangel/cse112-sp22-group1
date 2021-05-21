0081aaa957c9142bfa0a4ca591149316e65d7fd4
Waynar Bocangel Calderon <waynarbocangel@gmail.com>
2021-05-21 17:40:56 -0400

    remove unnecesary routes from api

--- codebase/back-end/index.js
+++ codebase/back-end/index.js
@@ -6,11 +6,8 @@ const createUser = require(__dirname + "/createFiles/createUser.js");
 const updateUser = require(__dirname + "/updateFiles/updateUser.js");
 const readUser = require(__dirname + "/readFiles/readUser.js");
 const security = require(__dirname + "/security/securityFunctions.js");
-const path = require("../front-end");
 
 const app = express();
-
-app.use(express.static(__dirname + "/front-end"));
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
@@ -23,15 +20,6 @@ mongoose.set("useCreateIndex", true);
 
 app.listen("3000", () => {
 	console.log("server has started listening to port 3000");
-});
-
-
-app.get("/login", (req, res) => {
-	res.sendFile(path.resolve("/login/login.js"));
-});
-
-app.get("/success", (req, res) => {
-	res.send("success");
 });
 
 app.post("/createUser", express.json({type: "*/*"}), (req, res) =>{
