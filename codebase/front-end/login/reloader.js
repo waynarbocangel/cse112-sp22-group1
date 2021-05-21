4885b5ea5902af942a7c5ff9fb166cec8b2a0bfc
Waynar Bocangel Calderon <waynarbocangel@gmail.com>
2021-05-21 03:00:45 -0400

    fixed login bug at reload when logged in

--- codebase/front-end/login/reloader.js
+++ codebase/front-end/login/reloader.js
@@ -3,7 +3,7 @@ import {readUser} from "../localStorage/userOperations.js";
 readUser((user) => {
 	console.log(user);
 	if (user.email != undefined){
-		window.location.href = "http://localhost:3000/success";
+		window.location.href = "http://localhost:8080/success";
 	} else {
 		console.log(user);
 	}
