9415457fdbc03524d612d6569c7628aecf56f55c
darres heyes <56111676+partimeVillain@users.noreply.github.com>
2021-05-24 20:30:32 -0700

    merge conflicts

--- codebase/back-end/createUser.js
+++ codebase/back-end/createUser.js
@@ -30,3 +30,34 @@ var makeUser = function(email,pwd) {
 module.exports = {
 	makeUser: makeUser
 }
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const schema = require(__dirname + "/schema.js");
+
+mongoose.connect(process.env.DB, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+function makeUser (email, pwd) {
+
+	const userObject = {
+		email: email,
+		pwd: pwd
+	};
+
+	const newUser = new schema.User({
+		userData: userObject
+	});
+
+	newUser.save((err, us) => {
+		if (err) {
+			return console.error(err);
+		}
+		return us;
+	});
+
+}
+
+module.exports = {
+	makeUser: makeUser
+}
