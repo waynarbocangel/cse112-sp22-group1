23ca187d8a89ff3c1d46b1b4985b262f911f199f
darres heyes <56111676+partimeVillain@users.noreply.github.com>
2021-05-25 18:56:20 -0700

    added babel and puppeteer config

---
+++ codebase/front-end/__tests__/test1.test.js
@@ -0,0 +1,44 @@
+//import * as oper from "../localStorage/userOperations";
+//const rq = require ('pouchDB');
+
+/*
+test('creating a user', () => {
+     operation.createUser('email', 'pawd', (response)=>{
+          console.log(response);
+     });
+});*/
+/*
+describe("testing pushToHistory ", ()=>  {
+      
+     test('1 + 1 = 2',()=>{
+          expect(2).toBe(2);
+     });
+ 
+ 
+//createMonthlyLog(parent, content, days, trackers, date, callback)
+     /*
+    oper.createUser('123', 'adsf', (e)=>{
+          var user1 = e;
+      });
+
+     test('adding weird page', ()=>{
+
+
+
+          let startDate1 = new Date(2021,5,24);
+          let end1 = new Date(2021,5,25);
+          months1 = ['May'];
+          content1 = ['abc'];
+          trackers1 =['abc1'];
+          let futureLog;
+          createFutureLog(startDate1, endDate1, months1, content1, trackers1,  (err, response)=>{
+               if(err){
+                    console.log(err);
+               } else {
+              futureLog = response;
+               }
+          });
+          expect(expected.content).toBe(futureLog.content);
+      });
+      */
+   //});
