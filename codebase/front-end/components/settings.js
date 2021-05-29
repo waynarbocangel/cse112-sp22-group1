1b772c2113060cce9facdba759dfe4eb73327532
Thomas Fischer <tfischer@ucsd.edu>
2021-05-29 03:18:32 -0700

    Themes: It works. It just works.

--- codebase/front-end/components/settings.js
+++ codebase/front-end/components/settings.js
@@ -2,11 +2,12 @@ const themeColors = {
     darkmode: {
         "--border-color": "#292929",
         "--content-foreground-color": "#D4D4D4",
-        "--content-background-color": "#1E1E1E",
+        "--content-background-color": "#2e3030",
         "--tracker-foreground-color": "#FFFFFF",
-        "--tracker-background-color": "#2B2D42",
-        "--navbar-foreground-color": "#FFFFFF",
-        "--navbar-background-color": "#333333",
+        "--tracker-background-color": "#1e2020", 
+        "--tracker-border-color":     "#404040",
+        "--navbar-foreground-color":  "#FFFFFF",
+        "--navbar-background-color":  "#1e2020",
         "--icon-filter": "invert()",
     },
 
@@ -15,11 +16,64 @@ const themeColors = {
         "--content-foreground-color": "#000000",
         "--content-background-color": "#FFFFFF",
         "--tracker-foreground-color": "#FFFFFF",
+        "--tracker-border-color":     "#7671B6",
         "--tracker-background-color": "#2B2D42",
-        "--navbar-foreground-color": "#FFFFFF",
-        "--navbar-background-color": "#F7F2EC",
+        "--navbar-foreground-color":  "#FFFFFF",
+        "--navbar-background-color":  "#F7F2EC",
         "--icon-filter": "",
-    }
+    },
+
+    highcontrast: {
+        "--border-color": "#F2F2F2",
+        "--content-foreground-color": "#FFFFFF",
+        "--content-background-color": "#000000",
+        "--tracker-foreground-color": "#FFFFFF",
+        "--tracker-background-color": "#0000FF",
+        "--tracker-border-color":     "#FFFFFF",
+        "--navbar-foreground-color":  "#FFFFFF",
+        "--navbar-background-color":  "#008F00",
+        "--icon-filter": "invert()",
+    },
+
+    theme4: {
+        "--border-color": "#F2F2F2",
+        "--content-foreground-color": "#000000",
+        "--content-background-color": "#FFFFFF",
+        "--tracker-foreground-color": "#FFFFFF", //"#9FEDD7",
+        "--tracker-background-color": "#026670",
+        "--tracker-border-color":     "#7671B6",
+        "--navbar-foreground-color":  "#FEF9C7",
+        "--navbar-background-color":  "#FCE181",
+        "--icon-filter": "",
+    },
+
+    theme5: {
+        "--border-color": "#8E8D8A",
+        "--content-foreground-color": "#000000",
+        "--content-background-color": "#EAE7DC",
+        
+        "--tracker-foreground-color": "#FFFFFF", //"#9FEDD7",
+        "--tracker-background-color": "#48131A",
+        "--tracker-border-color":     "#6F474E",
+
+        "--navbar-foreground-color":  "#000000",
+        "--navbar-background-color":  "#D8C3A5",
+        "--icon-filter": "",
+    },
+
+    theme6: {
+        "--border-color": "#34375c", 
+        "--content-foreground-color": "#AAABB8",
+        "--content-background-color": "#25274D",
+        
+        "--tracker-foreground-color": "#AAABB8",
+        "--tracker-background-color": "#283D6C", //283d6c
+        "--tracker-border-color":     "#29648A",
+
+        "--navbar-foreground-color":  "#000000",
+        "--navbar-background-color":  "#464866",
+        "--icon-filter": "invert()",
+    },
 }
 
 export class SettingsMenu extends HTMLElement {
@@ -35,6 +89,15 @@ export class SettingsMenu extends HTMLElement {
     connectedCallback() {
         this.shadowRoot.innerHTML = `
             <style>
+                @font-face {
+                    font-family:"SF-Pro";
+                    src: url("./public/fonts/SF-Pro.ttf");
+                }
+
+                :host {
+                    font-family: "SF-Pro";
+                }
+
                 #overlay {
                     position: fixed;
                     width: 100%;
@@ -92,6 +155,10 @@ export class SettingsMenu extends HTMLElement {
                     <form id="theme">
                         <input type="radio" name="themeradio" id="darkmode"> <label>Darkmode</label></input><br>
                         <input type="radio" name="themeradio" id="lightmode"> <label>Noobmode</label></input><br>
+                        <input type="radio" name="themeradio" id="highcontrast"> <label>High Contrast</label></input><br>
+                        <input type="radio" name="themeradio" id="theme4"> <label>Theme 4</label></input><br>
+                        <input type="radio" name="themeradio" id="theme5"> <label>Theme 5</label></input><br>
+                        <input type="radio" name="themeradio" id="theme6"> <label>Theme 6</label></input><br>
                     </form>
                 </div>
             </div>
