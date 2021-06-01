ad7a0926c860d30fcfe0ebc4cb36edc55284a5fe
claudia <chandoyo@ucsd.edu>
2021-06-01 14:40:55 -0700

    testing

--- codebase/front-end/__tests__/navbar.test.js
+++ codebase/front-end/__tests__/navbar.test.js
@@ -1,6 +1,6 @@
 import { TrackerMenu} from "../components/tracker.js"
 
-
+// Puppeteer Automated Testing
 
 describe('Basic user flow for SPA ', () => {
     
@@ -10,10 +10,12 @@ describe('Basic user flow for SPA ', () => {
       await page.goto('localhost:8080');
       await page.waitForTimeout(500);
     });
-*/
-   test('1 == 1?', ()=>{
+  */
+
+  
+  test('1 == 1?', ()=>{
     expect(1).toBe(1);
-   });
+  });
 
 
 
