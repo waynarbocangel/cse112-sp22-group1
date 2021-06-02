32ad4afd28bcc3646c1e18283cb3f1c15023d17d
streda <samuelreda2018@gmail.com>
2021-06-02 09:02:52 -0700

    added some more tests

--- codebase/unitTests/__tests__/block.test.js
+++ codebase/unitTests/__tests__/block.test.js
@@ -10,21 +10,39 @@ describe('Testing the functionality of text block', () => {
         });
     });
      
-      test('Check classlist property is updated correctly when changed to header 1', ()=>{
-            textBlock.setupHeader1();
-            expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header1')).toBe(true);
-      });
+    test('Check classlist property is updated correctly when changed to header 1', ()=>{
+        textBlock.setupHeader1();
+        expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header1')).toBe(true);
+    });
 
-       test('Check classlist property is updated correctly when changed to header 2', ()=>{
-            textBlock.setupHeader2();
-            expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header2')).toBe(true);
-      });
+    test('Check classlist property is updated correctly when changed to header 2', ()=>{
+        textBlock.setupHeader2();
+        expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header2')).toBe(true);
+    });
     
-     test('Check classlist property is updated correctly when changed to header 3', ()=>{
-            textBlock.setupHeader3();
-            expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header3')).toBe(true);
-      });
-   
-  });
+    test('Check classlist property is updated correctly when changed to header 3', ()=>{
+        textBlock.setupHeader3();
+        expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header3')).toBe(true);
+    });
+    test('Check classlist property is updated correctly when changed to Note', ()=>{
+        textBlock.setupNote();
+        expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('note')).toBe(true);
+    });
+    test('Check classlist property is updated correctly when changed to Task', ()=>{
+        textBlock.setupTask();
+        expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('task')).toBe(true);
+        expect(textBlock.shadowRoot.getElementById('textBlock').getAttribute("placeholder")).toEqual("Task");
+    });
+    test('Check classlist property is updated correctly when changed to setupEvent', ()=>{
+        textBlock.setupEvent();
+        expect(textBlock.shadowRoot.getElementById('textBlock').getAttribute("placeholder")).toEqual("Event:");
+        expect(textBlock.shadowRoot.getElementById('textBlock').getAttribute("dateFiller")).toEqual(" use @ for time HH:MM and # for weekdays or dates MM/DD/YY");
+    });
+    test('Check classlist property is updated correctly when changed to setupEvent', ()=>{
+        textBlock.removeStyles();
+        expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('unstylized')).toBe(true);
+        expect(textBlock.shadowRoot.getElementById('editorIcons').classList.contains('paragraphIcons')).toBe(true);
+    });
+})
 
   
