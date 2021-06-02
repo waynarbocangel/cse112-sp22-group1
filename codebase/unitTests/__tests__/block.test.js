78e40397e2a084b271229e4608a8bfe18d23995b
streda <samuelreda2018@gmail.com>
2021-06-01 17:06:38 -0700

    Restructured the testing functions a little bit for a better understanding

--- codebase/unitTests/__tests__/block.test.js
+++ codebase/unitTests/__tests__/block.test.js
@@ -2,35 +2,29 @@ import {TextBlock} from '../components/block.js'
 import {Controller} from '../components/blockController.js'
 
 describe('Testing the functionality of text block', () => {
-    
+    let controller; 
+    let textBlock;
+    beforeAll(()=>{
+        controller = new Controller(null, null, null);
+        textBlock = new  TextBlock(controller, null, function (success) {
+        });
+    });
      
       test('Check classlist property is updated correctly when changed to header 1', ()=>{
-          let controller = new Controller(null, null, null);
-          let textBlock = new  TextBlock(controller, null, function (success) {
-          });
-    
             textBlock.setupHeader1();
             expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header1')).toBe(true);
       });
 
-       test('Check classlist property is updated correctly when changed to header 1', ()=>{
-          let controller = new Controller(null, null, null);
-          let textBlock = new  TextBlock(controller, null, function (success) {
-          });
-    
+       test('Check classlist property is updated correctly when changed to header 2', ()=>{
             textBlock.setupHeader2();
             expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header2')).toBe(true);
       });
     
-     test('Check classlist property is updated correctly when changed to header 1', ()=>{
-          let controller = new Controller(null, null, null);
-          let textBlock = new  TextBlock(controller, null, function (success) {
-          });
-    
+     test('Check classlist property is updated correctly when changed to header 3', ()=>{
             textBlock.setupHeader3();
             expect(textBlock.shadowRoot.getElementById('textBlock').classList.contains('header3')).toBe(true);
       });
-    
+   
   });
 
   
