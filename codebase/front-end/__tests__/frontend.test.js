0967084bc398a5c75ef8d6abacc87d8f11e031ca
claudia <chandoyo@ucsd.edu>
2021-06-02 17:18:14 -0700

    rename test1 to frontend

---
+++ codebase/front-end/__tests__/frontend.test.js
@@ -0,0 +1,19 @@
+// Puppeteer
+
+describe('Basic user flow for SPA ', () => {
+    
+    beforeAll(async () => {
+      await page.goto('localhost:8080');
+      await page.waitForTimeout(500);
+    });
+
+    // Testing test
+    test(' 1 and 1', ()=>{
+      expect(1).toBe(1);
+    });
+    
+    
+
+
+
+});
