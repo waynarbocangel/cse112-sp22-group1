9e1fce7896a00306c86e5237aec5e0b1fbd16715
claudia <chandoyo@ucsd.edu>
2021-06-04 17:25:20 -0700

    check homepage

--- codebase/front-end/__tests__/frontend.test.js
+++ codebase/front-end/__tests__/frontend.test.js
@@ -12,6 +12,19 @@ describe('Basic user flow for SPA ', () => {
       expect(1).toBe(1);
     });
 
+    // Test 1
+    it('Test1: Initial Home Page - Check Home Page', async () => {
+      /*const numEntries = await page.$$eval('journal-entry', (entries) => {
+        return entries.length;
+      });
+      */
+      const title = await page.$eval("h1", (header) => { return header.innerHTML; });
+      expect(title).toBe("My Journal");
+      
+    });
+
+
+    
     // Test Navigation
     test('Create Future Log', () => {
       // TODO
