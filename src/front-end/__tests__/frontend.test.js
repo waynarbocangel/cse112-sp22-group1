940ccfb8242a647ea408fbe67a2487c6ae20bb6e
claudia <chandoyo@ucsd.edu>
2021-06-06 14:20:29 -0700

    Test Create New User Fails

--- src/front-end/__tests__/frontend.test.js
+++ src/front-end/__tests__/frontend.test.js
@@ -16,21 +16,32 @@ describe('Basic user flow for SPA ', () => {
     });
 
     // Test 2
-    it('Create New User Fails', async () => {
-      // Check that alert shows & url
+    it('Create New User Fails For Existing User', async () => {
+      await page.$eval('#email-field', el => el.value = 'test30');
+      await page.$eval('#password-field', pw => pw.value = 'qwert');
+      page.click('login-form-create');
+
       expect(page.url()).toBe("http://localhost:8080/login/");
     });
 
-    /*
+    
     // Test 3
     it('Log in', async () => {
       // fill in fields, click login, check link
 
-      
+      /*
+      document.getElementById("email-field").value = "test30";
+      document.getElementById("password-field").value = "qwert";
+      await page.click("login-form-submit");
+      */
+    
+      await page.goto('http://localhost:8080');
       //await page.click("login-form-submit");
+      expect(page.url()).toBe("http://localhost:8080/");
 
     });
 
+    /*
 
  
     test('Create Future Log', () => {
