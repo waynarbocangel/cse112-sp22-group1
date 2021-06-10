95ee46e55e08a387b5073617571a2c0c6bdc50ce
darres heyes <56111676+partimeVillain@users.noreply.github.com>
2021-06-09 23:48:04 -0700

    tests cases to 3

--- src/unitTests/__tests__/frontend.test.js
+++ src/unitTests/__tests__/frontend.test.js
@@ -1,19 +1,20 @@
 // Puppeteer
-jest.setTimeout(30000);
+jest.setTimeout(35000);
 
-describe('Basic user flow for SPA ', async () => {
+describe('Basic user flow for SPA ', () => {
     
-    beforeAll(async (done) => {
+    beforeAll(async () => {
       await page.goto('http://localhost:8080/login');
-      //await page.waitForTimeout(50000);
-      done();
+      await page.waitForTimeout(1000);
     });
+
 
     // Testing test
     test(' 1 and 1', async ()=>{
       expect(1).toBe(1);
-    }, 10000);
+    });
 
+    
     
     // Test 1
     it('Test1: Initial Home Page - Check Home Page', async () => {
@@ -28,52 +29,61 @@ describe('Basic user flow for SPA ', async () => {
       
       // check that alert shows & url
       
-      page.on('dialog', async dialog => {
+        page.on('dialog', async dialog => {
           console.log('here');
           await dialog.accept();
         });
         await page.click('input#login-form-create');
-
-        //await page.waitFor("#login-error-msg");
-        //await page.click("#BoxAlertBtnOk");
-    
-      expect(page.url()).toBe("http://localhost:8080/login/");
+        await page.click('input#email-field');
+        await page.type('input#email-field','test10');
+        await page.click('input#login-form-create');
+        await page.click('input#password-field');
+        await page.click('input#login-form-create');
+        expect(page.url()).toBe("http://localhost:8080/login/");
     });
 
     // Test 3
     it('Log in fail', async () => {
         // fill in fields, click login, check link
-        //await page.waitForSelector('email-field');
-
-        
-        
-        /*
-        await page.click('input#email-field');
-        await page.click('input#email-field');
+        //await page.waitForSelector('email-field');   
+        await page.click('input#email-field', {clickCount:3});
         await page.type('input#email-field','test10');
-        await page.click('input#password-field');
+        await page.click('input#password-field',{clickCount:3});
         await page.type('input#password-field','qwer3');
         await page.click("input#login-form-submit");
         await page.waitForTimeout(5000);
         expect(page.url()).toBe("http://localhost:8080/login/");
-        */
+    
         });
 
+    
     /*
     // Test 4
     it('Log in', async () => {
       // fill in fields, click login, check link
       //await page.waitForSelector('email-field');
-      await page.click('input#email-field');
+      await page.click('input#email-field', {clickCount:3});
       await page.type('input#email-field','test3');
-      await page.click('input#password-field');
+      await page.click('input#password-field',{clickCount:3});
       await page.type('input#password-field','qwert');
       await page.click("input#login-form-submit");
+      //await page.waitForNavigation();
       await page.waitForTimeout(5000);
-      expect(page.url()).toBe("http://localhost:8080/login/");
-    });
+      //let pageHeader = document.querySelector("topbar").shadowRoot.querySelector("#inner")
+
+
+      //console.log(pageHeader.title);
+
+      
+    }, 5000);
     */
 
+    //Test 5
+    /*
+    it('user', async()=>{
+
+    });
+    */
 
     /*
     test('Create Future Log', () => {
@@ -90,5 +100,5 @@ describe('Basic user flow for SPA ', async () => {
 
   
     */
-
+    
 });
--- src/unitTests/jest-puppeteer.config.js
+++ src/unitTests/jest-puppeteer.config.js
@@ -1,8 +1,7 @@
 // jest-puppeteer.config.js
 module.exports = {
   launch: {
-    testTimeout: 10000,
     headless: false,
-    slowMo: 500
+    slowMo: 10
   }
 }
