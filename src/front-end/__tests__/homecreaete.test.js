af34ae7d5f981ec8b811952e1344318c4722c7f2
claudia <chandoyo@ucsd.edu>
2021-06-10 22:18:05 -0700

    only contains home and create tests

---
+++ src/front-end/__tests__/homecreaete.test.js
@@ -0,0 +1,38 @@
+// Puppeteer
+
+jest.setTimeout(35000);
+
+describe('Basic user flow for SPA ', () => {
+
+    beforeAll(async () => {
+        await page.goto('http://localhost:8080/login');
+        await page.waitForTimeout(1000);
+    });
+
+    
+    // Test 1
+    it('Test1: Initial Home Page - Check Home Page', async () => {
+      // Remember to change this after deploy
+      expect(page.url()).toBe("http://localhost:8080/login/");
+      
+    });
+
+    // Test 2
+    it('Create New User Fails', async () => {
+      
+      // check that alert shows & url
+      
+        page.on('dialog', async dialog => {
+          await dialog.accept();
+        });
+        await page.click('input#login-form-create');
+        await page.click('input#email-field');
+        await page.type('input#email-field','test10');
+        await page.click('input#login-form-create');
+        await page.click('input#password-field');
+        await page.click('input#login-form-create');
+        expect(page.url()).toBe("http://localhost:8080/login/");
+    });
+
+});
+
