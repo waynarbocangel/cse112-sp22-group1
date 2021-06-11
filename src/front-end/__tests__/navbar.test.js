5935c3eb3d59b878f6b8ca00d3207d1bef5cd355
claudia <chandoyo@ucsd.edu>
2021-06-10 19:31:53 -0700

    test move

--- src/front-end/__tests__/navbar.test.js
+++ src/front-end/__tests__/navbar.test.js
@@ -1,5 +1,27 @@
-describe("testing test", () => {
-    test("tester", () => {
-		expect(1).toBe(1);
-	})
+import {NavBar} from '../components/navbar.js'
+
+
+
+
+describe('Basic user flow for SPA ', () => {
+    
+
+  /*
+    beforeAll(async () => {
+      await page.goto('localhost:8080');
+      await page.waitForTimeout(500);
+    });
+*/
+   
+    test('toggleTracker test', ()=>{
+        let navBar = new  NavBar();
+    
+        console.log(navBar.menu.classList.contains('menuClosed'));
+        navBar.toggleMenu();
+        console.log(navBar.menu.classList.contains('menuClosed'));
+        
+    });
+
+
+
 });
