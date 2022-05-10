3f9456d1ecb0a0bdc200d3e7e9c4a7ef79ea858a
Waynar Bocangel Calderon <waynarbocangel@Waynars-MBP.lan>
2022-05-10 15:13:58 -0700

    Not built test to check rejection

--- src/front-end/constants.js
+++ src/front-end/constants.js
@@ -1,2 +1,2 @@
-export let api = window.location.hostname == "boojo.bitfrost.app" ? "https://boojo.bitfrost.app/api" : "http://localhost:2827";
-export let origin = window.location.hostname == "boojo.bitfrost.app" ? "https://boojo.bitfrost.app" : "http://localhost:8080";
+export let api = window.location.hostname === "boojo.bitfrost.app" ? "https://boojo.bitfrost.app/api" : "http://localhost:2827";
+export let origin = window.location.hostname === "boojo.bitfrost.app" ? "https://boojo.bitfrost.app" : "http://localhost:8080";
