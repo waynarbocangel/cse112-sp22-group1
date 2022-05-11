353aebfd9318bef5fa0a1f3aa2adb271f370a2bf
forschoolandnothingelse <lsack@ucsd.edu>
2022-05-11 14:20:05 -0700

    backend: changes backend to use session cookies
    The previous implementation of the backend saved passwords in plaintext
    on the clientside. This was really bad for a number of reasons, not
    least of which is it just didn't work on a ton of browsers.
    
    The new backend uses an /auth route to first authenticate a user. It
    expects a JSON object of the form:
    {
        email: user@email.com,
        pwd: qwerty
    }
    From here it either authenticates the user's session and returns a JSON
    object with the "error" field set to null, or does not authenticate the
    user's session and returns an object with the "error" field set to an
    error string.
    
    This also changes the way the /{create,read,update,delete}User routes
    works. Now all user routes are just under /user. The way we do CRUD for
    users is using HTTP methods, POST for create, GET for read, PUT for
    update, and DELETE for delete. This should enforce not making every API
    call a POST for no good reason.
    
    This also enforces better security since a user can only modify their
    own user entry as determined for the session rather than the user
    themselves.
    
    The way user encryption works changes as well. Previously because the
    user always sent their password, the password was used as the key. Now a
    special encryption key is saved. The key is a simple encrypt-then-MAC of
    the user's password and the sessionSecret. This will break all
    previously stored data in Bujo, but makes for better security in the
    long run.
    
    Of course, we need to add an envvar for the sessionSecret under the key
    SESSION_SECRET, or deployment to production will fail.

--- src/back-end/readFiles/readUser.js
+++ src/back-end/readFiles/readUser.js
@@ -3,74 +3,71 @@
  * @namespace mongoRead
  */
 require("dotenv").config();
-const schema = require(__dirname + "/../schema.js");
-const security = require(__dirname + "/../security/securityFunctions.js");
+const schema = require(`${__dirname}/../schema.js`);
+const security = require(`${__dirname}/../security/securityFunctions.js`);
 
 /**
  * Reads the user from the online db and sends it to the callback.
  * @memberof mongoRead
- * @param {Object} userData The user data object to read from the online db.
+ * @param {String} email The email of the user to read.
+ * @param {String} key The decryption key to the user's data.
  * @callback (response) Either sends the user as an object from the online db or and error, if there is one, to the callback.
  */
-function readUser (userData, callback) {
-	schema.User.findOne({email: userData.email}, (error, user) => {
+const readUser = (email, key, callback) => {
+	schema.User.findOne({ email: email }, (error, user) => {
 		if (error) {
 			callback(error);
 		} else {
 			let newCollections = [];
 			for (let i = 0; i < user.collections.length; i++) {
 				let collection = user.collections[i];
-				collection.title = security.decrypt(collection.title, userData.pwd);
+				collection.title = security.decrypt(collection.title, key);
 				newCollections.push(collection);
 			}
 			let newTextBlocks = [];
 			for (let i = 0; i < user.textBlocks.length; i++) {
 				let block = user.textBlocks[i];
-				block.text = security.decrypt(block.text, userData.pwd);
+				block.text = security.decrypt(block.text, key);
 				newTextBlocks.push(block);
 			}
 			let newTasks = [];
 			for (let i = 0; i < user.tasks.length; i++) {
 				let block = user.tasks[i];
-				block.text = security.decrypt(block.text, userData.pwd);
+				block.text = security.decrypt(block.text, key);
 				newTasks.push(block);
 			}
 			let newEvents = [];
 			for (let i = 0; i < user.events.length; i++) {
 				let block = user.events[i];
-				block.title = security.decrypt(block.title, userData.pwd);
+				block.title = security.decrypt(block.title, key);
 				newEvents.push(block);
 			}
 			let newSignifiers = [];
 			for (let i = 0; i < user.signifiers.length; i++) {
 				let signifier = user.signifiers[i];
-				console.log(signifier.meaning);
-				console.log(security.decrypt(signifier.meaning, userData.pwd));
-				signifier.meaning = security.decrypt(signifier.meaning, userData.pwd);
+				signifier.meaning = security.decrypt(signifier.meaning, key);
 				newSignifiers.push(signifier);
 			}
 			let newImageBlocks = [];
 			for (let i = 0; i < user.imageBlocks.length; i++) {
 				let imageBlock = user.imageBlocks[i];
-				imageBlock.data = security.decrypt(imageBlock.data, userData.pwd);
+				imageBlock.data = security.decrypt(imageBlock.data, key);
 				newImageBlocks.push(imageBlock);
 			}
 			let newAudioBlocks = [];
 			for (let i = 0; i < user.audioBlocks.length; i++) {
 				let audioBlock = user.audioBlocks[i];
-				audioBlock.data = security.decrypt(audioBlock.data, userData.pwd);
+				audioBlock.data = security.decrypt(audioBlock.data, key);
 				newAudioBlocks.push(audioBlock);
 			}
 			let newTrackers = [];
 			for (let i = 0; i < user.trackers.length; i++) {
 				let tracker = user.trackers[i];
-				tracker.title = security.decrypt(tracker.title, userData.pwd);
+				tracker.title = security.decrypt(tracker.title, key);
                 newTrackers.push(tracker);
-                console.log("Testing Build Process");
 			}
 			let decodedUser = {
 				email: user.email,
-				pwd: userData.pwd,
 				index: user.index,
 				theme: user.theme,
 				dailyLogs: user.dailyLogs,
