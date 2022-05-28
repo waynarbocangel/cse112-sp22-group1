811b5eea0993fef28014dc993d2faaf5f5912eef
James Yuntae Lee <y2lee@ucsd.edu>
2022-05-27 19:22:17 -0700

    back-end/test: support for memory server testing
    To support npm commands, the directory's name no longer contains
    underscores. The necessary configuration files and packages have been
    added to support testing on in-memory servers. A sample tester file has
    also been added to ensure the testing server behaves properly before
    further tests are written.
    New commit with proper lint fixes.

---
+++ src/back-end/test/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "globals": {
+        "describe": false,
+        "beforeAll": false,
+        "afterAll": false,
+        "it": false,
+        "expect": false
+    }
+}
---
+++ src/back-end/test/sample.test.js
@@ -0,0 +1,27 @@
+const { MongoClient } = require("mongodb");
+
+describe("insert", () => {
+    let connection = null;
+    let db = null;
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        db = await connection.db(globalThis.__MONGO_DB_NAME__);
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    // Basic Test for Checking User Existence
+    it("should insert a doc into collection", async () => {
+        const users = db.collection("users");
+        const mockUser = {_id: "some-user-id", name: "John"};
+        await users.insertOne(mockUser);
+        const insertedUser = await users.findOne({_id: "some-user-id"});
+        expect(insertedUser).toEqual(mockUser);
+    });
+});
