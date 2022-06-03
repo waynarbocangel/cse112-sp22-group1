a67faf3bd422cc28b3555b14c32216d68cc2b0d6
Waynar Bocangel Calderon <waynarbocangel@gmail.com>
2022-06-02 23:46:35 -0700

    src/back-end fixes cors issue

--- src/back-end/constants.js
+++ src/back-end/constants.js
@@ -2,7 +2,7 @@
  * Module defining constants to be used in test environments.
  */
 module.exports = {
-    accessControlOrigin: "http://localhost:2827",
+    accessControlOrigin: "http://localhost:8080",
     sessionSecret: "wA531Y0qA3dhU8lxHElEhmomOM7P42WA",
     sessionCookieObject: {
         path: "/",
