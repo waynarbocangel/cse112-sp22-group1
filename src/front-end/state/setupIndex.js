081f86908a444a9b70da323d1212d7ada41bdbfb
Ehson Pirouzian <45323743+Ehson12@users.noreply.github.com>
2022-06-06 21:40:58 -0700

    frontend/setupIndex: lint fixes

--- src/front-end/state/setupIndex.js
+++ src/front-end/state/setupIndex.js
@@ -1,10 +1,7 @@
 import * as localStorage from "../localStorage/userOperations.js";
 import { contentWrapper, header } from "../index.js"
-import { CreatorBlock } from "../components/creator.jsx";
-import { DropdownBlock } from "../components/dropdown.jsx";
-import { FileLocation } from "../components/fileLocation.jsx";
-import { currentState } from "./stateManager.js";
 import { IndexDropdown } from "../components/indexDropdown.jsx";
+import { currentState } from "./stateManager.js";
 
 /**
  * Sets up the index page with the futureLogs and collections of the user.
@@ -25,15 +22,12 @@ export function setupIndex () {
 			let dropdownContainer = document.createElement("section");
 			dropdownContainer.id = "dropdownContainer";
 			contentWrapper.appendChild(dropdownContainer);
-		
+
 			for (let i = 0; i < parentArr.length; i++) {
 				console.log("inside for loop");
 				if (parentArr[i].objectType === "futureLog") {
-					let futureLogStart = new Date(parentArr[i].startDate);
-					let futureLogEnd = new Date(parentArr[i].endDate);
 					let dropdown = new IndexDropdown(parentArr[i]);
 					dropdownContainer.appendChild(dropdown)
-
 				}
 			}
 		}
