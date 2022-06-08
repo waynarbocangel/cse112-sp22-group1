9d8879ff4e9074bcf43962243af3b8e6b83995df
Ehson Pirouzian <45323743+Ehson12@users.noreply.github.com>
2022-06-07 23:02:37 -0700

    frontend/components/themes: added new themes

--- src/front-end/components/themePanel.jsx
+++ src/front-end/components/themePanel.jsx
@@ -1,25 +1,6 @@
 import * as localStorage from "../localStorage/userOperations.js";
 const themeColors = {
-    darkmode: {
-        "--border-color": "#4d4d4d",
-        "--content-background-color": "#2e3030",
-        "--content-foreground-color": "#D4D4D4",
-
-        "--tracker-background-color": "#1e2020",
-        "--tracker-foreground-color": "#d4d4d4",
-        "--tracker-border-color": "#404040",
-
-        "--navbar-background-color": "#1e2020",
-        "--navbar-foreground-color": "#d4d4d4",
-
-        "--dropdown-background-color": "#1e2020",
-        "--dropdown-foreground-color": "#d4d4d4",
-        "--dropdown-hover-color": "#474a4a",
-
-        "--icon-filter": "invert()"
-    },
-
-    lightmode: {
+    minimalLight: {
         "--body-background": "#FFFFFF",
         "--sidebar-background": "#FCFCFCFF",
         "--normal-font-color": "#000000",
@@ -51,82 +32,247 @@ const themeColors = {
         "--dropdown-hover-color": "#dfdcd8"
     },
 
-    highcontrast: {
-        "--border-color": "#F2F2F2",
-        "--content-background-color": "#000000",
-        "--content-foreground-color": "#FFFFFF",
+    coolDark: {
+        "--body-background": "#1C1C1CFF",
+        "--sidebar-background": "rgba(42, 42, 42, 1)",
+        "--normal-font-color": "#FFFFFFFF",
 
-        "--tracker-background-color": "#0000FF",
-        "--tracker-foreground-color": "#FFFFFF",
-        "--tracker-border-color": "#FFFFFF",
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(0, 153, 204, 1)",
+        "--secondary-button-color": "rgba(255, 136, 0, 1)",
 
-        "--navbar-background-color": "#008F00",
-        "--navbar-foreground-color": "#FFFFFF",
+        "--danger-button-color": "rgba(200, 0, 0, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(255, 255, 255, 1)",
 
-        "--dropdown-background-color": "#008F00",
-        "--dropdown-foreground-color": "#ffffff",
-        "--dropdown-hover-color": "#FF00FF",
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(42, 42, 42, 1)",
+        "--card-background-dim": "rgb(36, 36, 36)",
+        
+        "--card-background-dimmer": "rgb(32, 32, 32)",
+        "--divider-color": "rgba(255, 255, 255, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
 
-        "--icon-filter": "invert()"
+        "--calendar-font-color": "rgba(96, 96, 96, 1)",
+        "--calendar-today-font-color": "rgba(255, 255, 255, 1)",
+        "--sidebar-font-color": "rgba(255, 255, 255)",
+        "--card-font-color": "rgb(255, 255, 255)"
     },
 
-    theme4: {
-        "--border-color": "#F2F2F2",
-        "--content-background-color": "#FFFFFF",
-        "--content-foreground-color": "#000000",
+    theLightSide: {
+        "--body-background": "#F3F3F3FF",
+        "--sidebar-background": "#F3F3F3FF",
+        "--normal-font-color": "#000000FF",
 
-        "--tracker-background-color": "#026670",
-        "--tracker-foreground-color": "#FFFFFF",
-        "--tracker-border-color": "#1d7d86",
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(46, 155, 255, 1)",
+        "--secondary-button-color": "rgba(10, 184, 59, 1)",
 
-        "--navbar-background-color": "#FCE181",
-        "--navbar-foreground-color": "#fed442",
+        "--danger-button-color": "rgba(249, 49, 84, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(255, 255, 255, 1)",
 
-        "--dropdown-background-color": "#FFFFFF",
-        "--dropdown-foreground-color": "#000000",
-        "--dropdown-hover-color": "#efeded",
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(248, 248, 248, 1)",
+        "--card-background-dim": "rgba(232, 232, 232, 1)",
+        
+        "--card-background-dimmer": "rgba(216, 216, 216, 1)",
+        "--divider-color": "rgba(0, 0, 0, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
 
-        "--icon-filter": ""
+        "--calendar-font-color": "rgba(0, 0, 0, 1)",
+        "--calendar-today-font-color": "rgba(255, 255, 255, 1)",
+        "--sidebar-font-color": "rgb(0, 0, 0)",
+        "--card-font-color": "rgb(0, 0, 0)"
     },
 
-    theme5: {
-        "--border-color": "#8E8D8A",
-        "--content-background-color": "#EAE7DC",
-        "--content-foreground-color": "#000000",
+    theDarkSide: {
+        "--body-background": "#262626FF",
+        "--sidebar-background": "#242424FF",
+        "--normal-font-color": "#FFFFFFFF",
 
-        "--tracker-background-color": "#48131A",
-        "--tracker-foreground-color": "#FFFFFF",
-        "--tracker-border-color": "#6F474E",
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(200, 0, 0, 1)",
+        "--secondary-button-color": "rgba(190, 190, 190, 1)",
 
-        "--navbar-background-color": "#D8C3A5",
-        "--navbar-foreground-color": "#000000",
+        "--danger-button-color": "rgba(200, 0, 0, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(0, 0, 0, 1)",
 
-        "--dropdown-background-color": "#D8C3A5",
-        "--dropdown-foreground-color": "#000000",
-        "--dropdown-hover-color": "#c8ae89",
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(34, 34, 34, 1)",
+        "--card-background-dim": "rgb(28, 28, 28)",
+        
+        "--card-background-dimmer": "rgb(24, 24, 24)",
+        "--divider-color": "rgba(255, 255, 255, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
 
-        "--icon-filter": ""
+        "--calendar-font-color": "rgba(96, 96, 96, 1)",
+        "--calendar-today-font-color": "rgb(255, 255, 255)",
+        "--sidebar-font-color": "rgb(255, 255, 255)",
+        "--card-font-color": "rgb(255, 255, 255)"
     },
 
-    theme6: {
-        "--border-color": "#34375c",
-        "--content-background-color": "#1A1A3D",
-        "--content-foreground-color": "#E3E3E8",
+    snazzyLight: {
+        "--body-background": "#F4F4F8FF",
+        "--sidebar-background": "#FE4A49FF",
+        "--normal-font-color": "#000000FF",
 
-        "--tracker-background-color": "#25274D",
-        "--tracker-foreground-color": "#E3E3E8",
-        "--tracker-border-color": "#464866",
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(42, 183, 202, 1)",
+        "--secondary-button-color": "rgba(254, 215, 102, 1)",
 
-        "--navbar-background-color": "#25274D",
-        "--navbar-foreground-color": "#000000",
+        "--danger-button-color": "rgba(249, 49, 84, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(0, 0, 0, 1)",
 
-        "--content-placeholder-color": "#505b84",
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(230, 230, 234, 1)",
+        "--card-background-dim": "rgba(216, 216, 218, 1)",
+        
+        "--card-background-dimmer": "rgba(200, 200, 202, 1)",
+        "--divider-color": "rgba(0, 0, 0, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
 
-        "--dropdown-background-color": "#D8C3A5",
-        "--dropdown-foreground-color": "#000000",
-        "--dropdown-hover-color": "#c8ae89",
+        "--calendar-font-color": "rgba(0, 0, 0, 1)",
+        "--calendar-today-font-color": "rgba(255, 255, 255, 1)",
+        "--sidebar-font-color": "rgb(0, 0, 0)",
+        "--card-font-color": "rgb(0, 0, 0)"
+    },
 
-        "--icon-filter": "invert()"
+    highContrast: {
+        "--body-background": "#1C1C1CFF",
+        "--sidebar-background": "#00FF00FF",
+        "--normal-font-color": "#FFFFFFFF",
+
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(255, 0, 0, 1)",
+        "--secondary-button-color": "rgba(255, 0, 255, 1)",
+
+        "--danger-button-color": "rgba(255, 0, 0, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(255, 255, 255, 1)",
+
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(255, 255, 0, 1)",
+        "--card-background-dim": "rgba(239, 239, 0, 1)",
+        
+        "--card-background-dimmer": "rgba(223, 223, 202, 1)",
+        "--divider-color": "rgba(255, 255, 255, 1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.3)",
+
+        "--calendar-font-color": "rgba(0, 0, 0, 1)",
+        "--calendar-today-font-color": "rgba(255, 255, 255, 1)",
+        "--sidebar-font-color": "rgb(0, 0, 0)",
+        "--card-font-color": "rgb(0, 0, 0)"
+    },
+
+    comfyPanda: {
+        "--body-background": "#FCEBDEFF",
+        "--sidebar-background": "#FE9C8FFF",
+        "--normal-font-color": "#000000FF",
+
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(255, 121, 103, 1)",
+        "--secondary-button-color": "rgba(210, 178, 154, 1)",
+
+        "--danger-button-color": "rgba(249, 49, 84, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(255, 255, 255, 1)",
+
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(250, 215, 188, 1)",
+        "--card-background-dim": "rgba(234, 199, 172, 1)",
+        
+        "--card-background-dimmer": "rgba(218, 183, 156, 1)",
+        "--divider-color": "rgba(0, 0, 0, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
+
+        "--calendar-font-color": "rgba(0, 0, 0, 1)",
+        "--calendar-today-font-color": "rgba(255, 255, 255, 1)",
+        "--sidebar-font-color": "rgb(0, 0, 0)",
+        "--card-font-color": "rgb(0, 0, 0)"
+    },
+
+    nightOwl: {
+        "--body-background": "#112240FF",
+        "--sidebar-background": "#233554FF",
+        "--normal-font-color": "#FFFFFFFF",
+
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(73, 86, 112, 1)",
+        "--secondary-button-color": "rgba(204, 214, 246, 1)",
+
+        "--danger-button-color": "rgba(169, 0, 0, 1)",
+        "--primary-button-font-color": "rgba(255, 255, 255, 1)",
+        "--secondary-button-font-color": "rgba(0, 0, 0, 1)",
+
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(35, 53, 84, 1)",
+        "--card-background-dim": "rgba(28, 45, 76, 1)",
+        
+        "--card-background-dimmer": "rgba(20, 37, 68, 1)",
+        "--divider-color": "rgba(255, 255, 255, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
+
+        "--calendar-font-color": "rgba(96, 96, 96, 1)",
+        "--calendar-today-font-color": "rgba(255, 255, 255, 1)",
+        "--sidebar-font-color": "rgb(255, 255, 255)",
+        "--card-font-color": "rgb(255, 255, 255)"
+    },
+
+    sunnyBeach: {
+        "--body-background": "#FAF1EAFF",
+        "--sidebar-background": "#F9D199FF",
+        "--normal-font-color": "#000000FF",
+
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(187, 219, 247, 1)",
+        "--secondary-button-color": "rgba(249, 209, 153, 1)",
+
+        "--danger-button-color": "rgba(255, 71, 71, 1)",
+        "--primary-button-font-color": "rgba(0, 0, 0, 1)",
+        "--secondary-button-font-color": "rgba(0, 0, 0, 1)",
+
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(100, 171, 227, 1)",
+        "--card-background-dim": "rgba(84, 155, 211, 1)",
+        
+        "--card-background-dimmer": "rgba(66, 139, 195, 1)",
+        "--divider-color": "rgba(0, 0, 0, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
+
+        "--calendar-font-color": "rgba(255, 255, 255, 1)",
+        "--calendar-today-font-color": "rgba(0, 0, 0, 1)",
+        "--sidebar-font-color": "rgb(255, 255, 255)",
+        "--card-font-color": "rgb(255, 255, 255)"
+    },
+
+    darkForest: {
+        "--body-background": "#162C1BFF",
+        "--sidebar-background": "#223A27FF",
+        "--normal-font-color": "#FFFFFFFF",
+
+        "--highlighted-font-color": "#FFFFFFFF",
+        "--primary-button-color": "rgba(145, 88, 63, 1)",
+        "--secondary-button-color": "rgba(246, 227, 204, 1)",
+
+        "--danger-button-color": "rgba(169, 0, 0, 1)",
+        "--primary-button-font-color": "rgba(0, 0, 0, 1)",
+        "--secondary-button-font-color": "rgba(0, 0, 0, 1)",
+
+        "--card-shadow": "rgba(0, 0, 0, 0.25)",
+        "--card-background": "rgba(34, 58, 39, 1)",
+        "--card-background-dim": "rgba(26, 50, 31, 1)",
+        
+        "--card-background-dimmer": "rgba(22, 46, 27, 1)",
+        "--divider-color": "rgba(255, 255, 255, 0.1)",
+        "--lighter-divider-color": "rgba(140, 140, 140, 0.1)",
+
+        "--calendar-font-color": "rgba(255, 255, 255, 1)",
+        "--calendar-today-font-color": "rgba(0, 0, 0, 1)",
+        "--sidebar-font-color": "rgb(255, 255, 255)",
+        "--card-font-color": "rgb(255, 255, 255)"
     }
 }
 
@@ -140,23 +286,35 @@ import { createElement, createFragment } from "../jsxEngine.js";
 let template = <template>
 	<link type="text/css" rel="stylesheet" href="themePanel.css" />
     <form id="theme">
-        <label>Cool Dark <br />
-            <input type="radio" name="themeradio" id="darkmode" /><img class="themeImg" src="../../public/resources/coolDark.png" />
-        </label>
         <label>Minimal Light <br />
-            <input type="radio" name="themeradio" id="lightmode" /><img class="themeImg" src="../../public/resources/minimalLight.png" />
+            <input type="radio" name="themeradio" id="minimalLight" /><img class="themeImg" src="../../public/resources/minimalLight.png" />
         </label>
-        <label>High Contrast <br />
-            <input type="radio" name="themeradio" id="highcontrast" /><img class="themeImg" src="../../public/resources/highContrast.png" />
+        <label>Cool Dark <br />
+            <input type="radio" name="themeradio" id="coolDark" /><img class="themeImg" src="../../public/resources/coolDark.png" />
+        </label>
+        <label>The Light Side <br />
+            <input type="radio" name="themeradio" id="theLightSide" /><img class="themeImg" src="../../public/resources/highContrast.png" />
+        </label>
+        <label>The Dark Side <br />
+            <input type="radio" name="themeradio" id="theDarkSide" /><img class="themeImg" src="../../public/resources/snazzyLight.png" />
         </label>
         <label>Snazzy Light <br />
-            <input type="radio" name="themeradio" id="theme4" /><img class="themeImg" src="../../public/resources/snazzyLight.png" />
+            <input type="radio" name="themeradio" id="snazzyLight" /><img class="themeImg" src="../../public/resources/classyLight.png" />
         </label>
-        <label>Classy Light <br />
-            <input type="radio" name="themeradio" id="theme5" /><img class="themeImg" src="../../public/resources/classyLight.png" />
+        <label>High Contrast <br />
+            <input type="radio" name="themeradio" id="highContrast" /><img class="themeImg" src="../../public/resources/nightOwl.png" />
+        </label>
+        <label>Comfy Panda <br />
+            <input type="radio" name="themeradio" id="comfyPanda" /><img class="themeImg" src="../../public/resources/nightOwl.png" />
         </label>
         <label>Night Owl <br />
-            <input type="radio" name="themeradio" id="theme6" /><img class="themeImg" src="../../public/resources/nightOwl.png" />
+            <input type="radio" name="themeradio" id="nightOwl" /><img class="themeImg" src="../../public/resources/nightOwl.png" />
+        </label>
+        <label>Sunny Beach <br />
+            <input type="radio" name="themeradio" id="sunnyBeach" /><img class="themeImg" src="../../public/resources/nightOwl.png" />
+        </label>
+        <label>Dark Forest <br />
+            <input type="radio" name="themeradio" id="darkForest" /><img class="themeImg" src="../../public/resources/nightOwl.png" />
         </label>
     </form>
 </template>
